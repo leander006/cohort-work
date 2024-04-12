853c44b1053797fcc1c2db63114f5ae460517fec
leander006 <leanderdsilva06@gmail.com>
2024-04-12 23:30:05 +0530

    web socket server without socketio

---
+++ week-19/dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const ws_1 = require("ws");
+require("dotenv").config();
+const app = (0, express_1.default)();
+const httpServer = app.listen(process.env.PORT);
+const wss = new ws_1.WebSocketServer({ server: httpServer });
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === ws_1.WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+    ws.send('Hello! Message From Server!!');
+});

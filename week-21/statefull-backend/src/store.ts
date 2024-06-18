20e2269df592ed7b8cdd679cec294969ff2078e1
leander006 <leanderdsilva06@gmail.com>
2024-06-18 23:19:46 +0530

    few setup

--- week-21/statefull-backend/src/store.ts
+++ week-21/statefull-backend/src/store.ts
@@ -5,4 +5,20 @@ interface Games {
   moves: string[];
 }
 
-export const games: Games[] = [];
+// export const games: Games[] = [];
+
+export class GameManager {
+  games: Games[] = [];
+
+  constructor() {
+    this.games = [];
+  }
+
+  addMove(gameId: string, move: string) {
+    console.log(`Adding move ${move} in gameId ${gameId}`);
+    const game: any = this.games.find((games) => game.id === gameId);
+    game?.moves.push(move);
+  }
+
+  
+}

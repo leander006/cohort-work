59c3624dd02162dcb7cb84a86d85ac376e93b866
leander006 <leanderdsilva06@gmail.com>
2025-03-23 14:33:36 +0530

    unit testing

---
+++ week-24/unit test/vitest-testing/src/index.ts
@@ -0,0 +1,55 @@
+
+import express from "express";
+import { z } from "zod";
+import { prismaClient } from './__mock__/db'
+
+export const app = express();
+app.use(express.json());
+
+const sumInput = z.object({
+    a: z.number(),
+    b: z.number()
+})
+
+app.post("/sum", async(req:any, res:any) => {
+    const parsedResponse = sumInput.safeParse(req.body)
+    
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    await prismaClient.sum.create({
+        data:{
+            a:parsedResponse.data.a,
+            b:parsedResponse.data.b,
+            result:answer
+        }
+    })
+
+    res.json({
+        answer
+    })
+});
+
+app.get("/sum", (req:any, res:any) => {
+    const parsedResponse = sumInput.safeParse({
+        a: Number(req.headers["a"]),
+        b: Number(req.headers["b"])
+    })
+    
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    res.json({
+        answer
+    })
+});
